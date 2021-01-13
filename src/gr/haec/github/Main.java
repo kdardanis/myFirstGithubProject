830c59cbf8b04925a920c6a1b4e09eec35e32e56
kdardanis <kdardanis@gmail.com>
2021-01-13 20:03:14 +0200

    I change comments on main class

--- src/gr/haec/github/Main.java
+++ src/gr/haec/github/Main.java
@@ -3,6 +3,6 @@ package gr.haec.github;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My first github project!!!!!!");
+        System.out.println("My first github project!!!!!! What do you think?");
     }
 }
